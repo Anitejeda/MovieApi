77f98653ba907db1f2446937ecdfef81d13d9c64
Anitejeda <aniluztejeda1@gmail.com>
2023-05-18 16:45:34 -0400

    Adding feats

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.use(router);
 app.get('/', (req, res) => {
-    return res.send("Welcome to express!");
+    return res.send("Welcome to my personal Movies App API, Made by Ani Luz Tejeda");
 })
 
 // middlewares después de las rutas
